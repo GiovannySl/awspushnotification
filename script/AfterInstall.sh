8805cf5e8a3b7143f88d2dc7f55549956ae8596a
Giovanny Salazar <salazarl.giovanny@gmail.com>
2017-11-10 11:14:23 -0500

    Edit codedeploy files

--- script/AfterInstall.sh
+++ script/AfterInstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-cd /var/www/html
-wget https://s3-us-west-2.amazonaws.com/awspushnotification/.env
-source /usr/local/rvm/scripts/rvm
-rvm gemset use ruby-2.4.1@push
+#cd /var/www/html
+#wget https://s3-us-west-2.amazonaws.com/awspushnotification/.env
+#source /usr/local/rvm/scripts/rvm
+#rvm gemset use ruby-2.4.1@push
 #RAILS_ENV=development gem install bundler
-RAILS_ENV=development bundle install #--path vendor/bundle
+#RAILS_ENV=development bundle install #--path vendor/bundle
--- script/BeforeInstall.sh
+++ script/BeforeInstall.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #cd /var/www/html
 #lsof -ti:3000 | xargs kill
-cd /var/www
-rm -fr html
-mkdir html
+#cd /var/www
+#rm -fr html
+#mkdir html
