7baf0c83e1034b37cc210f106560bee4d6251cee
Giovanny Salazar <salazarl.giovanny@gmail.com>
2017-11-10 11:06:50 -0500

    Edit codedeploy files

--- script/ApplicationStart.sh
+++ script/ApplicationStart.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-#cd /var/www/html
-#source /usr/local/rvm/scripts/rvm
-#RAILS_ENV=development bundle exec rails server > /dev/null 2> /dev/null < /dev/null &
+cd /var/www/html
+source /usr/local/rvm/scripts/rvm
+RAILS_ENV=development bundle exec rails server > /dev/null 2> /dev/null < /dev/null &
