5941f9ca2bd7cd7f5b20c465b8cedd6c32652b30
Giovanny Salazar <salazarl.giovanny@gmail.com>
2017-11-14 08:43:44 -0500

    Fix time zone in text message

--- app/controllers/api/v1/notifications_controller.rb
+++ app/controllers/api/v1/notifications_controller.rb
@@ -16,7 +16,8 @@ class Api::V1::NotificationsController < Api::V1::BaseApiController
             when 200
                 if result[:item]["push_status"] && notification_params[:cell_phone] == result[:item]["cell_phone"]
                     client = Aws::SNS::Client.new(region: ENV['AWS_REGION'])
-                    message = "El siguiente es un mensaje de texto de prueba solicitado el #{Time.now.strftime("%m/%d/%Y a las %H:%M")} para #{result[:item]["email"]} al número +#{result[:item]["cell_phone"]}."
+                    local_time : Time.now.getlocal('-05:00').strftime("%m/%d/%Y a las %H:%M")
+                    message = "El siguiente es un mensaje de texto de prueba solicitado el #{local_time} para #{result[:item]["email"]} al número +#{result[:item]["cell_phone"]}."
                     client.publish({
                         phone_number: result[:item]["cell_phone"],
                         message: message,
